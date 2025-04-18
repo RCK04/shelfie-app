925578c9368a5a84414ef2109928dbd2438eaff7
Erick <erickkan2004@gmail.com>
2025-04-17 22:17:35 -0300

    feat: Setup expo app

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+const { View, Text, StyleSheet } = require("react-native")
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home!</Text>
+        </View>
+    )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+
+})
