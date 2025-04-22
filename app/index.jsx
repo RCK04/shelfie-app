212c39bd795fa032949a6d2d6349c4a57b33d2fc
Erick <erickkan2004@gmail.com>
2025-04-21 22:34:25 -0300

    feat: Home page - Text, Image, View

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,20 @@
-const { View, Text, StyleSheet } = require("react-native")
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Logo from '../assets/img/logo_light.png';
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home!</Text>
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img} />
+
+            <Text style={styles.title}>The number 1</Text>
+            <Text style={{ marginTop: 10, marginBottom: 30 }}>
+                Reading List App
+            </Text>
+
+            <View style={styles.card}>
+                <Text>Hello, this is a card.</Text>
+            </View>
         </View>
     )
 }
@@ -11,5 +22,25 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+
+    img: {
+        marginVertical: 20,
+    },
+
+    card: {
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0, 0, 0, 0.1)',
+    },
 })
