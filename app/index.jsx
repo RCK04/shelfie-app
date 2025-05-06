e27710b00dd1b84ec61dc695d30788326994b47d
Erick <erickkan2004@gmail.com>
2025-05-05 21:20:33 -0300

    feat: spacer

--- app/index.jsx
+++ app/index.jsx
@@ -1,46 +1,52 @@
-import { StyleSheet, Text } from 'react-native';
-import { Link } from 'expo-router';
+import { StyleSheet, Text } from "react-native";
+import { Link } from "expo-router";
 
-import Logo from '../assets/img/logo_light.png';
-import ThemedView from '../components/ThemedView';
-import ThemedLogo from '../components/ThemedLogo';
+import Logo from "../assets/img/logo_light.png";
+import ThemedView from "../components/ThemedView";
+import ThemedLogo from "../components/ThemedLogo";
+import Spacer from "../components/Spacer";
 
 const Home = () => {
-    return (
-        <ThemedView style={styles.container}>
-            <ThemedLogo style={styles.img} />
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedLogo style={styles.img} />
 
-            <Text style={styles.title}>The number 1</Text>
-            <Text style={{ marginTop: 10, marginBottom: 30 }}>
-                Reading List App
-            </Text>
+      <Text style={styles.title}>The number 1</Text>
 
-            <Link href="/about" style={styles.link}>About page</Link>
-            <Link href="/contact" style={styles.link}>Contact page</Link>
-        </ThemedView>
-    )
-}
+      <Spacer height={10} />
+      <Text>Reading List App</Text>
+      <Spacer />
+
+      <Link href="/about" style={styles.link}>
+        About page
+      </Link>
+      <Link href="/contact" style={styles.link}>
+        Contact page
+      </Link>
+    </ThemedView>
+  );
+};
 
 export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    title: {
-        fontWeight: 'bold',
-        fontSize: 18,
-    },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
 
-    img: {
-        marginVertical: 20,
-    },
-    
-    link: {
-        marginVertical: 10,
-        borderBottomWidth: 1,
-    },
-})
+  img: {
+    marginVertical: 20,
+  },
+
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+});
