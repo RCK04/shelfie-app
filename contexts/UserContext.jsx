fe3477d7c478bdb0b047f6a0e4b83665f054c65c
Erick <erickkan2004@gmail.com>
2025-05-17 16:28:20 -0300

    feat: register and login logics

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from "react"
+import { account } from "../lib/appwrite";
+import { ID } from "react-native-appwrite";
 
 
 export const UserContext = createContext();
@@ -7,11 +9,31 @@ export function UserProvider({ children }) {
     const [user, setUser] = useState(null);
 
     async function login(email, password) {
-        
+        try {
+            await account.createEmailPasswordSession(
+                email,
+                password
+            )
+            const response = await account.get();
+            setUser(response);
+
+        } catch (error) {
+            throw Error(error.message);
+        }
     }
 
     async function register(email, password) {
-        
+        try {
+            await account.create(
+                ID.unique(),
+                email,
+                password
+            )
+            await login(email, password);
+
+        } catch (error) {
+            throw Error(error.message);
+        }
     }
 
     async function logout() {
