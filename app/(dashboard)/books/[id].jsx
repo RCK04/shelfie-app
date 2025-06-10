c1aef4b80ea341c38cb06b32fc195e9eefc64827
Erick <erickkan2004@gmail.com>
2025-06-09 22:37:20 -0300

    feat: fetch by book id

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -1,18 +1,51 @@
 import { StyleSheet } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { useBooks } from "../../../hooks/useBooks";
 
 import Spacer from "../../../components/Spacer";
 import ThemedView from "../../../components/ThemedView";
 import ThemedText from "../../../components/ThemedText";
 import ThemedCard from "../../../components/ThemedCard";
+import ThemedLoader from "../../../components/ThemedLoader";
 import ThemedButton from "../../../components/ThemedButton"
 
 
 const BookDetails = () => {
+    const [book, setBook] = useState(null);
+
     const { id } = useLocalSearchParams();
+    const { fetchBookById } = useBooks();
+
+    useEffect(() => {
+        async function loadBook() {
+            const bookData = await fetchBookById(id);
+            setBook(bookData);
+        }
+
+        loadBook();
+    }, [id])
+
+    if (!book) {
+        return (
+            <ThemedView safe={true} style={styles.container}>
+                <ThemedLoader />
+            </ThemedView>
+        )
+    }
+
     return (
         <ThemedView safe={true} style={styles.container}>
-            <ThemedText>Book Details - {id}</ThemedText>
+            <ThemedCard style={styles.card}>
+                <ThemedText style={styles.title}>{book.title}</ThemedText>
+                <ThemedText>Written by {book.author}</ThemedText>
+                <Spacer />
+
+                <ThemedText title={true}>Book description:</ThemedText>
+                <Spacer height={10} />
+
+                <ThemedText>{book.description}</ThemedText>
+            </ThemedCard>
         </ThemedView>
     )
 }
@@ -23,4 +56,11 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'stretch',
     },
+    card: {
+        margin: 20,
+    },
+    title: {
+        fontSize: 22,
+        marginVertical: 10,
+    }
 })
